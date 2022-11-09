8463ec5f01ce3dc661fe584343faba6056fc961d
joker69216 <hamaraad883@gmail.com>
2022-11-09 11:10:06 +0300

    Add ADD functionality to the form / Add Remove Functionality to the buttons

--- src/components/AddBookForm.jsx
+++ src/components/AddBookForm.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBooks } from '../Redux/Books/Books';
 
 function AddBookForm() {
+  const dispatch = useDispatch();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const id = Math.floor(Math.random() * 1000);
+    const book = {
+      id,
+      title,
+      author,
+    };
+    dispatch(addBooks(book));
+    event.target.title.value = '';
+    event.target.author.value = '';
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label htmlFor="title">
         Title
-        <input type="text" name="title" id="title" />
+        <input type="text" name="title" id="title" required />
       </label>
 
       <label htmlFor="author">
         Author
-        <input type="text" name="author" id="author" />
+        <input type="text" name="author" id="author" required />
       </label>
 
       <button type="submit">Add Book</button>
