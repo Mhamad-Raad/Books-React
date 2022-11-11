0a4250932f4fc25f05f51771e87de276eefd7d92
joker69216 <hamaraad883@gmail.com>
2022-11-11 19:20:44 +0300

    add some of the design onto it

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteBooks, deleteBookAPI } from '../Redux/Books/Books';
+import css from './Book.module.css';
 
-function Book({ title, author, id }) {
+function Book(
+  {
+    title,
+    author,
+    id,
+    category,
+  },
+) {
   const dispatch = useDispatch();
 
   const handleRemoveBook = () => {
@@ -12,16 +20,16 @@ function Book({ title, author, id }) {
     );
   };
   return (
-    <>
+    <div className={css.card}>
       <span>
-        {title}
-        {' '}
-        by
-        {' '}
-        {author}
+        <p className={css.category}>{ category }</p>
+        <h3 className={css.title}>{title}</h3>
+        <p className={css.author}>{author}</p>
+        <div className={css.buttons}>
+          <button type="button" onClick={handleRemoveBook} className={css.edits}>Remove</button>
+        </div>
       </span>
-      <button type="button" onClick={handleRemoveBook}>Remove</button>
-    </>
+    </div>
   );
 }
 
@@ -31,4 +39,5 @@ Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Book from './Book';
+import css from './BookList.module.css';
 
 function Booklist() {
   const books = useSelector((state) => state.books.books);
   return (
-    <ul>
+    <ul className={css.booksList}>
       {books?.map((book) => (
         <li key={book.item_id}>
-          <Book title={book.title} author={book.author} id={book.item_id} />
+          <Book
+            title={book.title}
+            author={book.author}
+            id={book.item_id}
+            category={book.category}
+          />
         </li>
       ))}
     </ul>
