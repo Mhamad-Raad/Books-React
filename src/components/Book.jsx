2eadfd1f1e9f6915e69c13d2920f054af3b62daa
joker69216 <hamaraad883@gmail.com>
2022-11-12 20:16:56 +0300

    update design

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { deleteBooks, deleteBookAPI } from '../Redux/Books/Books';
 import css from './Book.module.css';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Book(
   {
@@ -26,9 +28,31 @@ function Book(
         <h3 className={css.title}>{title}</h3>
         <p className={css.author}>{author}</p>
         <div className={css.buttons}>
+          <button type="button" className={css.edits}>Comments</button>
+          <div className={css.vLine} />
           <button type="button" onClick={handleRemoveBook} className={css.edits}>Remove</button>
+          <div className={css.vLine} />
+          <button type="button" className={css.edits}>Edits</button>
         </div>
       </span>
+      <div>
+        <span className={css.progressSection}>
+          <CircularProgressbar
+            className={css.progressBar}
+            value={40}
+          />
+          <div className={css.percentage}>
+            <p className={css.percentageNumber}>40%</p>
+            <p>Completed</p>
+          </div>
+          <div className={css.VlingeR} />
+          <div className={css.updateProgress}>
+            <h3>Current Chapter</h3>
+            <p>Chapter 17</p>
+            <button type="button" className={css.updateProgressBTN}>Update Progress</button>
+          </div>
+        </span>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,23 +6,27 @@ import css from './Header.module.css';
 function Header() {
   return (
     <div className={css.header}>
-      <h1 className={css.logo}>
-        Bookstore
-      </h1>
+
       <div className={css.header_left_nav}>
-        <ul>
-          <li>
-            <NavLink to="/" className={css.links}>
-              Books
-            </NavLink>
-          </li>
-          <li>
-            {' '}
-            <NavLink to="Categories" className={`${css.links} ${css.disabled}`}>
-              Categories
-            </NavLink>
-          </li>
-        </ul>
+        <div>
+          <h1 className={css.logo}>
+            Bookstore
+          </h1>
+          <ul>
+            <li>
+              <NavLink to="/" className={css.links}>
+                Books
+              </NavLink>
+            </li>
+            <li>
+              {' '}
+              <NavLink to="Categories" className={`${css.links} ${css.disabled}`}>
+                Categories
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
         <div className={css.profile_pic}>
           <FaSmileWink className={css.pica} />
         </div>
